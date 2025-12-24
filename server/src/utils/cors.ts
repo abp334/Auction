74d9110c3440d92aedc998add7a209a0ccd2543e
abp334 <timepass.5354@gmail.com>
2025-12-24 23:22:06 +0530

    ui changes

--- server/src/utils/cors.ts
+++ server/src/utils/cors.ts
@@ -9,6 +9,7 @@ export function createCorsOptions(): CorsOptions {
   const allowOrigins = originEnv
     ? [
         originEnv,
+        "https://clashbid.live",
         "https://auction-ten-sage.vercel.app",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
