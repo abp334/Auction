6bb7b83cfd60b0618434ed474bb47efedc20ae4f
abp334 <timepass.5354@gmail.com>
2026-06-01 23:52:30 +0530

    bug fixes regarding previous commit

--- server/src/utils/db.ts
+++ server/src/utils/db.ts
@@ -1,6 +1,15 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const connectionString = process.env.DATABASE_URL;
+if (!connectionString) {
+  throw new Error("DATABASE_URL environment variable is required");
+}
+
+const adapter = new PrismaPg({ connectionString });
 
 const prisma = new PrismaClient({
+  adapter,
   log:
     process.env.NODE_ENV === "development"
       ? ["warn", "error"]
